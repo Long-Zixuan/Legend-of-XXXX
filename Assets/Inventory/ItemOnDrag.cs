29f2b93613ba2315af4997c0b61f9d15b4740b09
Long-Zixuan <778275146@qq.com>
2024-11-02 00:28:23 +0800

    cp fixed inventory manager bug

--- Assets/Inventory/ItemOnDrag.cs
+++ Assets/Inventory/ItemOnDrag.cs
@@ -21,6 +21,7 @@ public class ItemOnDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDr
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        Debug.Log(eventData.pointerCurrentRaycast.gameObject.name+ " childcount:"+eventData.pointerCurrentRaycast.gameObject.transform.childCount);
         if (eventData.pointerCurrentRaycast.gameObject == null || eventData.pointerCurrentRaycast.gameObject.tag != "Slots")
         {
             transform.SetParent(oriParent);
@@ -28,17 +29,32 @@ public class ItemOnDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDr
         }
         else if(eventData.pointerCurrentRaycast.gameObject.name == "ItemImage")
         {
+            
+            
             transform.SetParent(eventData.pointerCurrentRaycast.gameObject.transform.parent.parent);
             transform.position = eventData.pointerCurrentRaycast.gameObject.transform.parent.position;
 
-            
+
             eventData.pointerCurrentRaycast.gameObject.transform.parent.position = oriParent.position;
             eventData.pointerCurrentRaycast.gameObject.transform.parent.SetParent(oriParent);
+            
         }
-        else
+        else//碰到是slot了
         {
-            transform.SetParent(eventData.pointerCurrentRaycast.gameObject.transform);
-            transform.position = eventData.pointerCurrentRaycast.gameObject.transform.position;
+            if (eventData.pointerCurrentRaycast.gameObject.transform.GetChild(0).gameObject.activeSelf)//有物品
+            {
+                transform.SetParent(oriParent);
+                transform.position = oriParent.position;
+            }
+            else
+            {
+                GameObject itsItem = eventData.pointerCurrentRaycast.gameObject.transform.GetChild(0).gameObject;
+                itsItem.transform.SetParent(oriParent);
+                itsItem.transform.position = oriParent.position;
+
+                transform.SetParent(eventData.pointerCurrentRaycast.gameObject.transform);
+                transform.position = eventData.pointerCurrentRaycast.gameObject.transform.position;
+            }
         }
         GetComponent<CanvasGroup>().blocksRaycasts = true;
     }
